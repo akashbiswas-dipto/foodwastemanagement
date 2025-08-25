c2d0bfd80581237e92870e1526ddc27e3855c363
Akash Biswas <n12371661@qut.edu.au>
2025-08-25 11:36:29 +1000

    TASK-32 Created a profile update form

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://3.106.253.108:5001', // live
+  baseURL: 'http://localhost:5001',
+  //baseURL: 'http://3.106.253.108:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
